24effa24be439f46367303a38164c963a91d6366
d.levitin <disa15@rambler.ru>
2020-01-12 21:47:24 +0300

    consoleproj

--- NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
@@ -1,29 +1,29 @@
 using NUnit.Framework;
-using OpenQA.Selenium.IE;
-using System.IO;
-using NUnitTestProject1.Helpers;
-using OpenQA.Selenium;
 
 namespace Tests
 {
-
     public class Tests
     {
-        private InternetExplorerDriver _driver;
         [SetUp]
         public void Setup()
         {
-            var path = Directory.GetCurrentDirectory();
-            var driverService = InternetExplorerDriverService.CreateDefaultService(path);
-            _driver = new InternetExplorerDriver(driverService);
         }
 
         [Test]
         public void Test1()
         {
-            Starter.OpenSite(_driver);
-
             Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void Test3()
+        {
+            Assert.Fail();
         }
     }
 }
