2292b477ccc867df7a042814be18b784faeb975a
Verlevina <36883393+Verlevina@users.noreply.github.com>
2020-01-18 21:52:14 +0300

    Update UnitTest1.cs

--- NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
@@ -9,21 +9,38 @@ namespace Tests
         {
         }
 
-        [Test]
+             [Test]
         public void Test1()
         {
-            Assert.Pass();
-        }
+            using (var conn = new SqlConnection())
+            {
+                string userName;
+                conn.ConnectionString = @"Data Source=DESKTOP-19GTHEH\SQLEXPRESS; Initial Catalog=ShootServ; Integrated Security=true;";
+                conn.Open();
+                using (var command = new SqlCommand(@"SELECT TOP (1000) [Id]
+                      ,[Name]
+                      ,[Family]
+                      ,[FatherName]
+                      ,[Login]
+                      ,[Password]
+                      ,[IdRole]
+                      ,[DateCreate]
+                      ,[E_mail]
+                  FROM [ShootServ].[dbo].[Users]", conn))
+                {
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        userName = reader["Name"].ToString();
+                    }
+                }
 
-        [Test]
-        public void Test2()
-        {
+            }
+
+
             Assert.Pass();
         }
-        [Test]
-        public void Test3()
-        {
-            Assert.Fail();
-        }
+    }
+}
     }
 }
